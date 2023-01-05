5edd1ba9f466813e9083fc3abab765bf1a5238c9
pmahon3 <pmahon3@uwo.ca>
2023-01-05 14:57:16 -0800

    setup update

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(name='EmpiricalDynamics',
       version='0.1.2',
       description='Modelling tools for nonlinear state space reconstruction analysis',
-      packages=find_packages(),
+      packages=['edynamics',
+                'edynamics.modelling_tools',
+                'edynamics.modelling_tools.blocks',
+                'edynamics.modelling_tools.data_types',
+                'edynamics.modelling_tools.models'],
       author='Patrick Mahon',
       author_email='pmahon3@uwo.ca',
       zip_safe=False)
