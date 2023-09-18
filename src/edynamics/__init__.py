12ff9c6b4af3d45cbb37cc3e4d413c7af892feb1
Patrick Mahon <pmahon@inn-development.cedar>
2023-09-18 13:20:34 +0000

    removing modules in development

--- src/edynamics/__init__.py
+++ src/edynamics/__init__.py
@@ -4,4 +4,4 @@ from .modelling_tools import norms
 from .modelling_tools import observers
 from .modelling_tools import optimizers
 from .modelling_tools import projectors
-from .modelling_tools import weighers
+from .modelling_tools import kernels
--- src/edynamics/modelling_tools/__init__.py
+++ src/edynamics/modelling_tools/__init__.py
@@ -2,6 +2,6 @@ from .embeddings import embedding
 from .estimators import dimensionality, nonlinearity
 from .norms import minkowski
 from .observers import lag, lag_moving_average
-from .optimizers import gnn_optimizer, monte_carlo_search_tree
+from .optimizers import gnn_optimizer
 from .projectors import knn, smap
-from .weighers import exponential, epanechnikov, tricubic, constant
+from .kernels import exponential, epanechnikov, tricubic, constant
